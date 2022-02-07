e92928e744e94a306ea4e7e0d7233247fbb4d60a
kerimkullov <libertyrx98@gmail.com>
2022-02-07 19:49:42 +0600

    Blocprovider and value

--- lib/main.dart
+++ lib/main.dart
@@ -49,64 +49,67 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            BlocListener<AnimatedContainerBlock, AnimatedContainerState>(
-              bloc: containerBlock,
-              listener: (context, state) {
-                print('BlocListener PRINT');
+            BlocProvider(
+              create: (context) => containerBlock,
+              child:
+                  BlocConsumer<AnimatedContainerBlock, AnimatedContainerState>(
+                      builder: (context, state) {
                 if (state is AnimatedContainerStateValue) {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => SeconPage(
-                        containerColor: state.containerColor.toString(),
+                  print('BLOCBUILDER PRINT');
+                  return Column(
+                    children: [
+                      AnimatedContainer(
+                        height: state.height,
+                        width: state.width,
+                        curve: Curves.easeInOutBack,
+                        duration: const Duration(seconds: 1),
+                        decoration: BoxDecoration(
+                          color: state.containerColor,
+                          borderRadius: BorderRadius.circular(16),
+                        ),
                       ),
-                    ),
+                      const SizedBox(height: 50),
+                      SizedBox(
+                        height: 50,
+                        width: 50,
+                        child: BlocProvider.value(
+                          value: containerBlock,
+                          child: SeconPage(),
+                        ),
+                      )
+                    ],
                   );
                 }
-              },
-              child:
-                  BlocBuilder<AnimatedContainerBlock, AnimatedContainerState>(
-                      bloc: containerBlock,
-                      builder: (context, state) {
-                        if (state is AnimatedContainerStateValue) {
-                          print('BLOCBUILDER PRINT');
-                          return AnimatedContainer(
-                            height: state.height,
-                            width: state.width,
-                            curve: Curves.easeInOutBack,
-                            duration: const Duration(seconds: 1),
-                            decoration: BoxDecoration(
-                              color: state.containerColor,
-                              borderRadius: BorderRadius.circular(16),
-                            ),
-                          );
-                        }
-                        return const Text('Что то пошло не так');
-                      }),
+                return const Text('Что то пошло не так');
+              }, listener: (context, state) {
+                if (state is AnimatedContainerStateValue) {
+                  print('BlocListener PRINT');
+                }
+              }),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          containerBlock.add(ChangeContainerBehavior());
-        },
-        tooltip: 'Generate',
-        child: const Icon(Icons.replay),
       ),
     );
   }
 }
 
 class SeconPage extends StatelessWidget {
-  const SeconPage({Key? key, required this.containerColor}) : super(key: key);
-  final String containerColor;
+  SeconPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text(containerColor),
+        child: FloatingActionButton(
+          onPressed: () {
+            context
+                .read<AnimatedContainerBlock>()
+                .add(ChangeContainerBehavior());
+          },
+          tooltip: 'Generate',
+          child: const Icon(Icons.replay),
+        ),
       ),
     );
   }
